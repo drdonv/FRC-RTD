a19c96c73483e23ff69e6d9113c0583c5c0e37fb
drdonv <realdrdonv@gmail.com>
2022-03-03 01:43:26 -0800

    Added terminology page, removed a few lumache calls

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FRC Made Easy'
+copyright = '2022, Dhanvi Ganti and Karthik Appana'
+author = 'drdonv, cartha353'
 
 release = '0.1'
 version = '0.1.0'
